f7775d48a673d0e2c01e7a642ac141126cac8c1d
Mirco Gonzalez <95445386+mircovippinn@users.noreply.github.com>
2022-09-16 13:09:23 -0300

    loading

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,8 +5,9 @@ import { Card } from "../card/Card";
 import style from './Cards.module.css'
 
 
-export const Cards = () => {
+export const Cards = ({setLoading}) => {
     const[post, setPost] = useState([]);
+    
 
     const getPosts = async () => {
         try {
@@ -17,6 +18,7 @@ export const Cards = () => {
               posts.push({ ...doc.data(), id: doc.id });
             });
             setPost(posts);
+            setLoading(false);
           });
         } catch (e) {
           console.error("No hay resultados disponibles:", e);
@@ -29,9 +31,16 @@ export const Cards = () => {
 
     return (
         <div className={style.containerCards}>
+            {post.length > 0 ? 
+            <>
             {post.length > 0 && post?.map((p, i) => (
                 <Card key={i} image={p.image} name={p.name} message={p.message} />
             ))}
+            </>
+            : <button class="btn btn-primary" type="button" disabled>
+            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            <span class="visually mr-1">Loading...</span>
+          </button>}
         </div>
     )
 }
